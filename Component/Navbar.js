327c841dc3045f03b2b0df9802a5366fcaf82bda
niharika sahu <121661609+nihu-13@users.noreply.github.com>
2024-11-27 22:44:08 +0530

    Initial commit

--- Component/Navbar.js
+++ Component/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
                 <div className="container-fluid">
-                  <h4 className='text-light'> Navbar</h4>  
+                  <h4 className='text-light'>The Hunter’s Path</h4>  
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
