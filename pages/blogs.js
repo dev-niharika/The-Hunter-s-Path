8fdcf41c6acec3dcd803b530842d5db06ab6489a
niharika sahu <121661609+nihu-13@users.noreply.github.com>
2024-11-29 23:38:49 +0530

    Devloye

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,35 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import Image from 'next/image'
-import code from '../public/code-stable.png'
-import Link from 'next/link'
-export default function blogs() {
+import fs from 'fs';
+import path from 'path';
+import Image from 'next/image';
+import Link from 'next/link';
+import code from '../public/code-stable.png';
 
-    const [blogs, setblogs] = useState([])
-    useEffect(() => {
-        fetch('http://localhost:3000/api/blog').then((a) => {
-            return a.json();
+export async function getStaticProps() {
+    const directory = path.join(process.cwd(), 'BlogData'); // Path to JSON files
+    const filenames = await fs.promises.readdir(directory);
+
+    const blogs = await Promise.all(
+        filenames.map(async (filename) => {
+            const filePath = path.join(directory, filename);
+            const fileContents = await fs.promises.readFile(filePath, 'utf8');
+            return JSON.parse(fileContents); // Parse JSON file
         })
-            .then((parsed) => {
-                setblogs(parsed)
-            })
-    }, []);
+    );
+
+    return {
+        props: {
+            blogs, // Pass data to the page as props
+        },
+    };
+}
+
+export default function Blogs({ blogs }) {
     return (
-        <><div class="row m-4">
-            {blogs.map((blogsit) => {
-                return <div class="col-sm-4">
-                        <div className="card"  key={blogsit.slug}>
-                            <Image src={code} width={40} height={100} className="card-img-top p-3 " alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">{blogsit.title}</h5>
-                                <p className="card-text">{blogsit.content.substr(0, 110)}</p>
-                                <Link href={`/Bloges/${blogsit.slug}`} className="btn btn-outline-dark btn-sm">Go somewhere</Link>
-                            </div>
+        <div className="row m-4">
+            {blogs.map((blog) => (
+                <div className="col-sm-4" key={blog.slug}>
+                    <div className="card">
+                        <Image
+                            src={code}
+                            width={40}
+                            height={100}
+                            className="card-img-top p-3"
+                            alt="Blog Image"
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">{blog.title}</h5>
+                            <p className="card-text">{blog.content.substr(0, 110)}...</p>
+                            <Link href={`/Bloges/${blog.slug}`}>
+                                <a className="btn btn-outline-dark btn-sm">Read More</a>
+                            </Link>
                         </div>
                     </div>
-            })}
-            </div>
-
-        </>
-    )
+                </div>
+            ))}
+        </div>
+    );
 }
- 
